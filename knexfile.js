6a62c16e27699a31f6db78cd759a43b237362469
Lospub <zhjyy002@gmail.com>
2024-12-17 11:32:42 -0700

    update packages and connect to knex

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,11 @@ export default {
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-  }
+  },
+  migrations: {
+    directory: './migrations',
+  },
+  seeds: {
+    directory: './seeds',
+  },
 };
